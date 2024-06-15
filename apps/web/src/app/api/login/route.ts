9ed1992de17b54918303490c2447c945a49a442e
Brice Duke <bricewduke@gmail.com>
2024-06-15 18:00:28 -0500

    fix(login): only insert users

--- apps/web/src/app/api/login/route.ts
+++ apps/web/src/app/api/login/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: Request) {
 		},
 	);
 
-	await supabase.from("users").upsert({
+	await supabase.from("users").insert({
 		id: user.wallet.address,
 		username: formatAddress(user.wallet.address),
 		image: "",
